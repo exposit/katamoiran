f59b761cda0e9950052a3b7aefb99a6b7479deab
exposit <alto.dizi@gmail.com>
2018-11-28 14:04:30 -0800

    site maintain

--- _plugins/xfeed.rb
+++ _plugins/xfeed.rb
@@ -15,41 +15,46 @@ module Jekyll
       #jekyll_coll = Jekyll::Collection.new(site, 'externalfeed')
       #site.collections['externalfeed'] = jekyll_coll
 
-    blogs = [  'http://udan-adan.blogspot.com/feeds/posts/default?alt=rss',
-          'https://coinsandscrolls.blogspot.com/feeds/posts/default?alt=rss',
-          'http://www.bastionland.com/feeds/posts/default?alt=rss', 'http://falsemachine.blogspot.com/feeds/posts/default',
-          'http://goblinpunch.blogspot.com/feeds/posts/default?alt=rss',
-          'http://hackslashmaster.blogspot.com/feeds/posts/default',
-          'http://necrotic-gnome-productions.blogspot.com/feeds/posts/default?alt=rss']
+    t = File.ctime("./_includes/side_roll.html")
 
-    fileHtml = File.new("./_includes/side_roll.html", "w+")
-    fileHtml.puts '<section id="sidebar-roll" class="color2 rounded side">'
-    fileHtml.puts '<div>'
-    fileHtml.puts '<header class="major">'
-    fileHtml.puts '<h3>Blogs I Read</h3>'
-    fileHtml.puts '</header>'
-    fileHtml.puts '<ul>'
+    if Time.now.utc - t > 2000
 
-    blogs.each do |e|
-        feed = Feedjira::Feed.fetch_and_parse(e)
-        title = feed.title
-        guid = feed.url
-        entry = feed.entries.first
-        etitle = entry.title
-        etitle = etitle.split[0...7].join(' ')
-        eurl = entry.url
-        pub = entry.published
-        now = Time.now.utc
-        #elapsed = time_ago_in_words(pub ) + ' ago' # out-dated almost immediately
-        elapsed = pub.strftime("%b %d, %Y") # use the publication time instead
+      blogs = [  'http://udan-adan.blogspot.com/feeds/posts/default?alt=rss',
+            'https://coinsandscrolls.blogspot.com/feeds/posts/default?alt=rss',
+            'http://www.bastionland.com/feeds/posts/default?alt=rss', 'http://falsemachine.blogspot.com/feeds/posts/default',
+            'http://goblinpunch.blogspot.com/feeds/posts/default?alt=rss',
+            'http://hackslashmaster.blogspot.com/feeds/posts/default',
+            'http://necrotic-gnome-productions.blogspot.com/feeds/posts/default?alt=rss']
 
-        fileHtml.puts '<li><a href="%s" target="_new">%s</a> | <a href="%s" target="_new">%s</a> | <cite>%s</cite></li>' % [guid, title, eurl, etitle, elapsed]
+      fileHtml = File.new("./_includes/side_roll.html", "w+")
+      fileHtml.puts '<section id="sidebar-roll" class="color2 rounded side">'
+      fileHtml.puts '<div>'
+      fileHtml.puts '<header class="major">'
+      fileHtml.puts '<h3>Blogs I Read</h3>'
+      fileHtml.puts '</header>'
+      fileHtml.puts '<ul>'
 
+      blogs.each do |e|
+          feed = Feedjira::Feed.fetch_and_parse(e)
+          title = feed.title
+          guid = feed.url
+          entry = feed.entries.first
+          etitle = entry.title
+          etitle = etitle.split[0...7].join(' ')
+          eurl = entry.url
+          pub = entry.published
+          now = Time.now.utc
+          #elapsed = time_ago_in_words(pub ) + ' ago' # out-dated almost immediately
+          elapsed = pub.strftime("%b %d, %Y") # use the publication time instead
+
+          fileHtml.puts '<li><a href="%s" target="_new">%s</a> | <a href="%s" target="_new">%s</a> | <cite>%s</cite></li>' % [guid, title, eurl, etitle, elapsed]
+
+
+        end
+
+        fileHtml.puts '</ul><div class="total-posts">... and about 300 more on <a href="https://www.inoreader.com/u/alto.dizi">innoreader</a></div></div></section>'
 
       end
-
-      fileHtml.puts '</ul><div class="total-posts">... and about 300 more on <a href="https://www.inoreader.com/u/alto.dizi">innoreader</a></div></div></section>'
-
     end
   end
 end
